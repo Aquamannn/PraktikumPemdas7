5e3e65dc3dc11059259f9b7acda4d733744a6cf5
Lenovo <9.3razzinayyuri@gmail.com>
2025-04-29 18:32:41 +0700

    Membuat Prosedure printData

--- oop.cpp
+++ oop.cpp
@@ -6,7 +6,12 @@ class Mahasisawa{
     int nim;
     string nama;
     float nilai;
-    
+    void printData(){
+        cout << "Nim :" << nim << endl;
+        cout << "Nama :" << nama << endl;
+        cout << "Nilai :" << nilai << endl;
+    }
+
 }
 
 int main(){
